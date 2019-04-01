6322eca9864ccdd70b85265ae7e7d4e2c6356312
sully martinez <sullymartinezj@gmail.com>
2019-03-31 22:03:29 -0400

    first commit

---
+++ public/main.js
@@ -0,0 +1,41 @@
+// var thumbUp = document.getElementsByClassName("fa-thumbs-up");
+var trash = document.getElementsByClassName("fa-trash");
+
+// Array.from(thumbUp).forEach(function(element) {
+//       element.addEventListener('click', function(){
+//         //
+//         const msg = this.parentNode.parentNode.childNodes[1].innerText
+//         fetch('messages', {
+//           method: 'put',
+//           headers: {'Content-Type': 'application/json'},
+//           body: JSON.stringify({
+//             'msg': msg,
+//           })
+//         })
+//         .then(response => {
+//           if (response.ok) return response.json()
+//         })
+//         .then(data => {
+//           console.log(data)
+//           window.location.reload(true)
+//         })
+//       });
+// });
+
+Array.from(trash).forEach(function(element) {
+      element.addEventListener('click', function(){
+        //childNodes is an input tag, referencing an odd #
+        const msg = this.parentNode.parentNode.childNodes[1].innerText
+        fetch('messages', {
+          method: 'delete',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            'msg': msg
+          })
+        }).then(function (response) {
+          window.location.reload()
+        })
+      });
+});
